894b436d81f348e70bb1363b2cde7cc57b196487
Marcin Koralewski <marcin@student.edu>
2016-12-22 11:06:00 +0100

    zrobiony crud. teraz musze zrobić coś co da mi możliwość komentarzy pod zadaniem.

---
+++ src/taskPlannerBundle/Form/TaskType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace taskPlannerBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('description')->add('done')->add('dueDate')->add('users')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'taskPlannerBundle\Entity\Task'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'taskplannerbundle_task';
+    }
+
+
+}
